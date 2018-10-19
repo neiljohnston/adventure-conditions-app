f85bacc9e1cc532f385980d2f7eb6b617c4d6d12
Neil Johnston <neil@neiljohnston.net>
2018-10-18 18:58:20 -0700

    loading external geojson

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -5,26 +5,62 @@
 </template>
 <script>
 import 'ol/ol.css';
+import axios from 'axios';
 
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
+import {
+  GeoJSON,
+  
+} from 'ol/format';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
+import {
+  Tile as TileLayer,
+  Image as ImageLayer,
+} from 'ol/layer';
+
+import {
+  ImageArcGISRest,
+  ImageWMS,
+  OSM,
+  XYZ,
+} from 'ol/source';
 
 import {
   defaults as defaultControls,
   ScaleLine,
 } from 'ol/control';
 
+import {
+  layerDefinitions,
+  navigationDefinitions,
+} from '../assets/js/layers';
+
+
 const scaleLineControl = new ScaleLine();
+
+let map;
+let mapView;
 
 export default {
   name: 'MapView',
 
   data() {
     return {
-      map: null,
-      mapView: null,
+      name: 'Adventure Conditions', // process.env.name,
+      tagline: 'Finding Safe and Healthy Stoke', // process.env.tagline,
+      hasLegends: true, // process.env.hasLegends
+      hasReader: false, // process.env.hasReader,
+      siteId: 'adventureconditions', // process.env.siteId,
+      description: 'Adventure conditions unifies evacuation orders and alerts, road closures, air quality data, smoke conditions and weather to help navigate BC Wildfires', // process.env.description,
+      keywords: 'California, fires, BC, British Columbia, Wildfires, Evacuations, Road Conditions, Smoke, Air Quality, Health', // process.env.keywords,
+      center: [-14173186.261234362, 7196206.431941464],
+      zoom: 5,
+
+      layers: [],
+      navigation: [],
     };
   },
 
@@ -37,41 +73,107 @@ export default {
   },
 
   mounted() {
-    this.map = new Map({
-      target: 'map',
-      controls: defaultControls({
-        attributionOptions: {
-          collapsible: false,
-        },
-      }).extend([
-        scaleLineControl,
-      ]),
-      loadTilesWhileAnimating: true,
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          }),
-        }),
-      ],
-    });
+    map = this.initializeMap();
+    mapView = this.updateMapView(this.center, this.zoom);
 
-    this.mapView = new View({
-      center: [0, 0],
-      zoom: 2,
-    });
-
-    this.map.setView(this.mapView);
+    map.setView(mapView);
     this.mapResizeHandler();
+
+    // initialize layers and navigation as reactive
+    this.layers = layerDefinitions;
+    this.navigation = navigationDefinitions;
+
+    this.initializeLayers(this.layers);
   },
 
   methods: {
-    mapResizeHandler() {
-      console.log('mapResizeHandler');
-      this.$nextTick(() => {
-        this.map.updateSize();
+    initializeMap() {
+      return new Map({
+        target: 'map',
+        controls: defaultControls({
+          attributionOptions: {
+            collapsible: false,
+          },
+        }).extend([
+          scaleLineControl,
+        ]),
+        loadTilesWhileAnimating: true,
+        layers: [
+          new TileLayer({
+            source: new XYZ({
+              url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            }),
+          }),
+        ],
       });
     },
+
+    updateMapView(center, zoom) {
+      return new View({
+        center: center,
+        zoom: zoom,
+      });
+    },
+
+    mapResizeHandler() {
+      this.$nextTick(() => {
+        map.updateSize();
+      });
+    },
+
+    initializeLayers(layers) {
+      layers.forEach((layer, index) => {
+        const zindex = layers.length - index;
+        console.log(layer.id, zindex);
+
+        if (layer.type === 'geojson') {
+          if (layer.visible) {
+            this.$set(layer, 'loadState', 'loading');
+          }
+          const newSource = new VectorSource({
+            format: new GeoJSON(),
+            loader: this.geoJsonLoader(layer),
+          });
+
+          this.$set(layer, 'source', newSource);
+
+          const newLayer = new VectorLayer({
+            source: layer.source,
+            visible: layer.visible,
+            opacity: layer.opacity,
+            // style: styleFunction,
+            zIndex: zindex,
+          });
+
+          this.$set(layer, 'layer', newLayer);
+
+          // layer.source.on('addfeature', (event) => {
+          //   const feature = event.feature;
+          //   feature.set('layerMapId', layer.id);
+          // });
+
+          map.addLayer(layer.layer);
+        }
+      });
+    },
+
+    geoJsonLoader(layer) {
+      axios.get(layer.endpoint, { timeout: 45000 })
+        .then((response) => {
+          this.$set(layer, 'loadState', '');
+          const projectionsConversion = (layer.dataProjection) ? { dataProjection: layer.dataProjection, featureProjection: 'EPSG:3857'} : {};
+          layer.source.addFeatures(
+            layer.source.getFormat().readFeatures(response.data, projectionsConversion)
+          );
+        })
+        .catch((e) => {
+          this.$set(layer, 'loadState', 'error');          
+          // this.setVisibility(layer.id, false);
+          // eslint-disable-next-line no-console
+          console.log('Load Error: ', e);
+        });
+    }
+
   },
 };
 </script>
