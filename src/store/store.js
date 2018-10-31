bf547faff94a3d33593392218a16ab9fe5b0e22f
Neil Johnston <neil@neiljohnston.net>
2018-10-31 11:23:16 -0700

    vuex persist navigation state

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 import {
   navigationDefinitions,
 } from '../assets/js/layers';
 
 Vue.use(Vuex);
+
+const vuexLocalStorage = new VuexPersistence({
+  key: 'vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  reducer: (state) => ({ navigation: state.navigation }),
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+});
 
 export default new Vuex.Store({
   state: {
@@ -26,6 +36,7 @@ export default new Vuex.Store({
     tilesArray: [],
     isSheetVisible: false,
   },
+  plugins: [vuexLocalStorage.plugin],
 
   getters: {
     getNavigationControlById: state => (id) => {
